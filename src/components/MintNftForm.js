7ee183c0029e92e4fad6b2f1a46795b724a2b0cd
nnthienbao <nnthienbao040@gmail.com>
2021-11-23 17:19:48 +0700

    improve nint nft form

--- src/components/MintNftForm.js
+++ src/components/MintNftForm.js
@@ -5,24 +5,91 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 
 export default function MintNftForm() {
+  const [fileUpload, setFileUpload] = useState(null);
+  const [fileName, setFileName] = useState("");
+  const [fileDes, setFileDes] = useState("");
+  const [errorFileUpload, setErrorFileUpload] = useState(false);
+  const [errorFileName, setErrorFileName] = useState(false);
+  const [errorFileDes, setErrorFileDes] = useState(false);
+
+  const mint = () => {
+    setErrorFileUpload(false);
+    setErrorFileName(false);
+    setErrorFileDes(false);
+    if (!fileUpload) {
+      setErrorFileUpload(true);
+    }
+    if (!fileName) {
+      setErrorFileName(true);
+    }
+    if (!fileDes) {
+      setErrorFileDes(true);
+    }
+    if (!errorFileUpload && !errorFileName && !errorFileDes) {
+      
+    }
+  };
+
   return (
-    <Box sx={{ mx: "auto", width: 400 }} style={{padding: 20}}>
+    <Box sx={{ mx: "auto", width: 400 }} style={{ padding: 20 }}>
       <Typography variant="subtitle1" component="div">
         File upload
       </Typography>
-      <TextField fullWidth required name="upload-photo" type="file" />
+      <TextField
+        error={errorFileUpload}
+        helperText={errorFileUpload ? "Please choose file upload" : ""}
+        fullWidth
+        required
+        name="upload-photo"
+        type="file"
+        id="fileUpload"
+        name="fileUpload"
+        onChange={(e) => setFileUpload(e.target.files[0])}
+      />
 
-      <Typography style={{marginTop: 10}} variant="subtitle1" component="div">
+      <Typography style={{ marginTop: 10 }} variant="subtitle1" component="div">
         Name
       </Typography>
-      <TextField fullWidth required name="name" type="input" placeholder="Enter NFT name..." />
+      <TextField
+        error={errorFileName}
+        helperText={errorFileName ? "Filename cannot be empty" : ""}
+        fullWidth
+        required
+        name="name"
+        type="input"
+        placeholder="Enter NFT name..."
+        value={fileName}
+        onChange={(e) => setFileName(e.target.value)}
+      />
 
-      <Typography style={{marginTop: 10}} variant="subtitle1" component="div">
+      <Typography style={{ marginTop: 10 }} variant="subtitle1" component="div">
         Description
       </Typography>
-      <TextField fullWidth required name="description" type="input" placeholder="Enter NFT description..." />
+      <TextField
+        error={errorFileDes}
+        helperText={errorFileDes ? "File description cannot be empty" : ""}
+        fullWidth
+        required
+        name="description"
+        type="input"
+        placeholder="Enter NFT description..."
+        value={fileDes}
+        onChange={(e) => setFileDes(e.target.value)}
+      />
 
-      <Button style={{display: 'block', marginLeft: 'auto', marginRight: 'auto', marginTop: 10}} variant="outlined" size="large">Mint</Button>
+      <Button
+        onClick={mint}
+        style={{
+          display: "block",
+          marginLeft: "auto",
+          marginRight: "auto",
+          marginTop: 10,
+        }}
+        variant="outlined"
+        size="large"
+      >
+        Mint
+      </Button>
     </Box>
   );
 }
